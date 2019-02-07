66698202161a471bf4661851e4b301f0412ca00e
Ishara Dayarathna <idayarathna@virtusa.com>
2019-02-07 15:00:58 +0530

    initial commit

---
+++ src/main/java/com/example/login/login/LoginApplication.java
@@ -0,0 +1,14 @@
+package com.example.login.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+ // https://medium.com/@gustavo.ponce.ch/spring-boot-spring-mvc-spring-security-mysql-a5d8545d837d
+}
+
---
+++ src/main/java/com/example/login/login/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.login.login.repository;
+
+import com.example.login.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
